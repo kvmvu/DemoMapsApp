0a16ddecf5636c339eef43e4a0bb18ae24a2a0bc
Chris Otaalo <john.otaalo@strathmore.edu>
2017-01-25 02:23:42 +0300

    Made some changes on the login API as well as the design of the dashboard and the sign up page

--- app/src/main/java/com/example/eric/demomapsapp/Login.java
+++ app/src/main/java/com/example/eric/demomapsapp/Login.java
@@ -1,25 +1,37 @@
 package com.example.eric.demomapsapp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.kosalgeek.android.md5simply.MD5;
 import com.kosalgeek.genasync12.AsyncResponse;
 import com.kosalgeek.genasync12.PostResponseAsyncTask;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private Button login;
+    private TextView login;
     private EditText etUserName, etPass, etCpass, etEmail;
     private DbHelper dbHelper;
     private Session session;
@@ -33,11 +45,18 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Sign Up");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         etUserName = (EditText) findViewById(R.id.txtUsername);
         etEmail = (EditText) findViewById(R.id.txtEmail);
         etPass = (EditText) findViewById(R.id.txtPassword);
         etCpass = (EditText) findViewById(R.id.txtPasswordConfirm);
-        login = (Button)findViewById(R.id.btnLogin);
+        login = (TextView)findViewById(R.id.btnLogin);
 
         login.setOnClickListener(this);
 
@@ -64,35 +83,64 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         data.put("textPassword", etPass.getText().toString());
         data.put("textPasswordConfirm", etCpass.getText().toString());
 
-        if(!(etUserName.equals("")) && (etEmail.equals("")) && (etPass.equals("")) && (etCpass.equals(""))){
-            PostResponseAsyncTask task = new PostResponseAsyncTask(Login.this, new AsyncResponse() {
+        if(etUserName.getText().toString() != "" && etEmail.getText().toString() != "" && etPass.getText().toString() != "" && etCpass.getText().toString() != ""){
+//            PostResponseAsyncTask task = new PostResponseAsyncTask(Login.this, new AsyncResponse() {
+//                @Override
+//                public void processFinish(String s) {
+//                    Log.d(TAG, s);
+//                    if(s.contains("Successfully")){
+//                        editor.putString("Username", etUserName.getText().toString());
+//                        editor.putString("Email Address", etEmail.getText().toString());
+//                        if(etPass.equals(etCpass)){
+//                            editor.putString("Password", MD5.encrypt(etPass.getText().toString()));
+//                            editor.putString("Password Confirm", MD5.encrypt(etCpass.getText().toString()));
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Confirm Password", Toast.LENGTH_SHORT).show();
+//                        }
+//                        editor.apply();
+//
+//                        Log.d(TAG, sharedPreferences.getString("Username", ""));
+//                        Log.d(TAG, sharedPreferences.getString("Email Address", ""));
+//                        Log.d(TAG, sharedPreferences.getString("Password", ""));
+//                        Log.d(TAG, sharedPreferences.getString("Password Confirm", ""));
+//
+//                        Toast.makeText(getApplicationContext(),"User Registration Successful", Toast.LENGTH_LONG).show();
+//                        Intent intent = new Intent(getApplicationContext(), Start.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                }
+//            });
+//            task.execute("http://savtech.co.ke/demomapsapp/register.php");
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Creating Account...");
+            progressDialog.show();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://savtech.co.ke/demomapsapp/register.php", new Response.Listener<String>() {
                 @Override
-                public void processFinish(String s) {
-                    Log.d(TAG, s);
-                    if(s.contains("Successfully")){
-                        editor.putString("Username", etUserName.getText().toString());
-                        editor.putString("Email Address", etEmail.getText().toString());
-                        if(etPass.equals(etCpass)){
-                            editor.putString("Password", MD5.encrypt(etPass.getText().toString()));
-                            editor.putString("Password Confirm", MD5.encrypt(etCpass.getText().toString()));
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Confirm Password", Toast.LENGTH_SHORT).show();
-                        }
-                        editor.apply();
-
-                        Log.d(TAG, sharedPreferences.getString("Username", ""));
-                        Log.d(TAG, sharedPreferences.getString("Email Address", ""));
-                        Log.d(TAG, sharedPreferences.getString("Password", ""));
-                        Log.d(TAG, sharedPreferences.getString("Password Confirm", ""));
-
-                        Toast.makeText(getApplicationContext(),"User Registration Successful", Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(getApplicationContext(), Start.class);
-                        startActivity(intent);
-                        finish();
-                    }
+                public void onResponse(String response) {
+                    progressDialog.hide();
+                    Toast.makeText(Login.this, response, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), Start.class);
+                    startActivity(intent);
                 }
-            });
-            task.execute("http://127.0.0.1/demomapsapp/register.php");
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                protected Map<String, String> getParams(){
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("textUsername", etUserName.getText().toString());
+                    params.put("textEmail", etEmail.getText().toString());
+                    params.put("textPassword", etPass.getText().toString());
+                    params.put("textPasswordConfirm", etCpass.getText().toString());
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
         } else {
             Toast.makeText(getApplicationContext(), "Please ensure all fields are filled out before signing up",
                     Toast.LENGTH_SHORT).show();
@@ -119,4 +167,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         }
     }*/
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }

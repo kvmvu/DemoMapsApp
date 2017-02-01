809148736be01f43fd7d0a00e3569919e8c560ac
Chris Otaalo <john.otaalo@strathmore.edu>
2017-02-01 22:54:20 +0300

    Made some changes on the login API as well as the design of the dashboard and the sign up page

--- app/src/main/java/com/example/eric/demomapsapp/Login.java
+++ app/src/main/java/com/example/eric/demomapsapp/Login.java
@@ -77,95 +77,45 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     //on sign up
     @Override
     public void onClick(View view) {
-        HashMap<String, String> data = new HashMap<String, String>();
-        data.put("textUsername", etUserName.getText().toString());
-        data.put("textEmail", etEmail.getText().toString());
-        data.put("textPassword", etPass.getText().toString());
-        data.put("textPasswordConfirm", etCpass.getText().toString());
 
-        if(etUserName.getText().toString() != "" && etEmail.getText().toString() != "" && etPass.getText().toString() != "" && etCpass.getText().toString() != ""){
-//            PostResponseAsyncTask task = new PostResponseAsyncTask(Login.this, new AsyncResponse() {
-//                @Override
-//                public void processFinish(String s) {
-//                    Log.d(TAG, s);
-//                    if(s.contains("Successfully")){
-//                        editor.putString("Username", etUserName.getText().toString());
-//                        editor.putString("Email Address", etEmail.getText().toString());
-//                        if(etPass.equals(etCpass)){
-//                            editor.putString("Password", MD5.encrypt(etPass.getText().toString()));
-//                            editor.putString("Password Confirm", MD5.encrypt(etCpass.getText().toString()));
-//                        } else {
-//                            Toast.makeText(getApplicationContext(), "Confirm Password", Toast.LENGTH_SHORT).show();
-//                        }
-//                        editor.apply();
-//
-//                        Log.d(TAG, sharedPreferences.getString("Username", ""));
-//                        Log.d(TAG, sharedPreferences.getString("Email Address", ""));
-//                        Log.d(TAG, sharedPreferences.getString("Password", ""));
-//                        Log.d(TAG, sharedPreferences.getString("Password Confirm", ""));
-//
-//                        Toast.makeText(getApplicationContext(),"User Registration Successful", Toast.LENGTH_LONG).show();
-//                        Intent intent = new Intent(getApplicationContext(), Start.class);
-//                        startActivity(intent);
-//                        finish();
-//                    }
-//                }
-//            });
-//            task.execute("http://savtech.co.ke/demomapsapp/register.php");
-
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setMessage("Creating Account...");
-            progressDialog.show();
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://savtech.co.ke/demomapsapp/register.php", new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    progressDialog.hide();
-                    Toast.makeText(Login.this, response, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), Start.class);
-                    startActivity(intent);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-
-                }
-            }){
-                protected Map<String, String> getParams(){
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("textUsername", etUserName.getText().toString());
-                    params.put("textEmail", etEmail.getText().toString());
-                    params.put("textPassword", etPass.getText().toString());
-                    params.put("textPasswordConfirm", etCpass.getText().toString());
-                    return params;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            requestQueue.add(stringRequest);
+        if(!etUserName.getText().toString().matches("") && !etEmail.getText().toString().matches("") && !etPass.getText().toString().matches("") && !etCpass.getText().toString().matches("")){
+            if (etPass.getText().toString().matches(etCpass.getText().toString())) {
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Creating Account...");
+                progressDialog.show();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://savtech.co.ke/demomapsapp/register.php", new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.hide();
+                        Toast.makeText(Login.this, response, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Start.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("textUsername", etUserName.getText().toString());
+                        params.put("textEmail", etEmail.getText().toString());
+                        params.put("textPassword", etPass.getText().toString());
+                        params.put("textPasswordConfirm", etCpass.getText().toString());
+                        return params;
+                    }
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+            }else{
+                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_LONG).show();
+            }
         } else {
             Toast.makeText(getApplicationContext(), "Please ensure all fields are filled out before signing up",
                     Toast.LENGTH_SHORT).show();
         }
     }
-    /*
-    public void register(View v){
-        String Username = etUserName.getText().toString();
-        String Email = etEmail.getText().toString();
-        String Password = etPass.getText().toString();
-        String confirmPassword = etCpass.getText().toString();
-        if(Email.isEmpty() && Password.isEmpty()){
-            Toast.makeText(getApplicationContext(), "Username/password is empty", Toast.LENGTH_SHORT).show();
-        }else {
-            if (!Password.equals(confirmPassword)) {
-                Toast.makeText(getApplicationContext(), "Please confirm correct password", Toast.LENGTH_SHORT).show();
-            } else {
-                dbHelper.addUser(Email, Password);
-                Session.setLoggedIn(true);
-                Toast.makeText(getApplicationContext(), "User Registered", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Login.this, Start.class));
-                finish();
-            }
-        }
-    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {

e066413b265f01aebe671998665b4846a6a4193c
kvmvu <kamauericm@gmail.com>
2017-02-14 19:14:51 +0300

    latest commit

--- app/src/main/java/com/example/eric/demomapsapp/SplashScreen.java
+++ app/src/main/java/com/example/eric/demomapsapp/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.eric.demomapsapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
 
